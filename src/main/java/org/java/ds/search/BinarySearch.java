3ebd06725262d0d489c45de4f4fe447a3ca0712e
Jeevan Patil <jeevan@jeevan-Vostro-2520.(none)>
2014-02-20 04:59:40 +1100

    code indentation

--- src/main/java/org/java/ds/search/BinarySearch.java
+++ src/main/java/org/java/ds/search/BinarySearch.java
@@ -4,29 +4,29 @@ package org.java.ds.search;
  * 
  * @author jeevan
  * @date 20-Feb-2014
- * @purpose Binary search implementation. 
- *
+ * @purpose Binary search implementation.
+ * 
  */
 public class BinarySearch {
-	public boolean contains(int[] arr, int searchNum) {
-		if(arr.length == 0) {
-			return false;
-		}
+    public boolean contains(int[] arr, int searchNum) {
+        if (arr.length == 0) {
+            return false;
+        }
 
-		int first = 0;
-		int last = arr.length - 1;
+        int first = 0;
+        int last = arr.length - 1;
 
-		while(first <= last) {
-			int middle = (first + last) / 2;
-			if(searchNum > arr[middle]) {
-				first = middle + 1;
-			} else if(searchNum < arr[middle]) {
-				last = middle - 1;
-			} else if(searchNum == arr[middle]) {
-				System.out.println(searchNum + " found at position " + (middle + 1));
-				return true;
-			}
-		}
-		return false;
-	}	
+        while (first <= last) {
+            int middle = (first + last) / 2;
+            if (searchNum > arr[middle]) {
+                first = middle + 1;
+            } else if (searchNum < arr[middle]) {
+                last = middle - 1;
+            } else if (searchNum == arr[middle]) {
+                System.out.println(searchNum + " found at position " + (middle + 1));
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- src/test/java/org/java/ds/search/BinarySearchTest.java
+++ src/test/java/org/java/ds/search/BinarySearchTest.java
@@ -5,14 +5,14 @@ import org.junit.Test;
 
 public class BinarySearchTest {
 
-	@Test
-	public void searchArray() {
-		BinarySearch binary = new BinarySearch();
-		int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-		Assert.assertTrue(binary.contains(array, 6));
-		Assert.assertTrue(binary.contains(array, 10));
-		Assert.assertTrue(binary.contains(array, 5));
-		Assert.assertFalse(binary.contains(array, 11));
-	}
+    @Test
+    public void searchArray() {
+        BinarySearch binary = new BinarySearch();
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        Assert.assertTrue(binary.contains(array, 6));
+        Assert.assertTrue(binary.contains(array, 10));
+        Assert.assertTrue(binary.contains(array, 5));
+        Assert.assertFalse(binary.contains(array, 11));
+    }
 
 }
