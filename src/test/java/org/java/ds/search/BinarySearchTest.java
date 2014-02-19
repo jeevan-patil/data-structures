9cab95b3dda9d68a3678d0a826a284f4005f77b7
Jeevan Patil <jeevan@jeevan-Vostro-2520.(none)>
2014-02-20 04:55:03 +1100

    Binary search class implementation and test case

---
+++ src/test/java/org/java/ds/search/BinarySearchTest.java
@@ -0,0 +1,18 @@
+package org.java.ds.search;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BinarySearchTest {
+
+	@Test
+	public void searchArray() {
+		BinarySearch binary = new BinarySearch();
+		int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		Assert.assertTrue(binary.contains(array, 6));
+		Assert.assertTrue(binary.contains(array, 10));
+		Assert.assertTrue(binary.contains(array, 5));
+		Assert.assertFalse(binary.contains(array, 11));
+	}
+
+}
