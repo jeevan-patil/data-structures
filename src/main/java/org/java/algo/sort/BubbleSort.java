ae73bce9d216490f9fcccd7cd11f9d211e0abff6
Jeevan Patil <jeevan@jeevan-Vostro-2520.(none)>
2014-02-21 03:53:05 +1100

    code formatted

--- src/main/java/org/java/algo/sort/BubbleSort.java
+++ src/main/java/org/java/algo/sort/BubbleSort.java
@@ -5,22 +5,22 @@ package org.java.algo.sort;
  * @author jeevan
  * @date 21-Feb-2014
  * @purpose Bubble sort implementation.
- *
+ * 
  */
 public class BubbleSort {
 
     public int[] bubbleSort(int[] array) {
-        if(array.length == 0) {
+        if (array.length == 0) {
             return array;
         }
 
         int size = array.length;
         int i = 0, j, k = 0, temp;
 
-        while(i < size -1) {
+        while (i < size - 1) {
             k = size - 1;
-            for(j = k; j > i; j--) {
-                if(array[j] < array[j - 1]) {
+            for (j = k; j > i; j--) {
+                if (array[j] < array[j - 1]) {
                     k = j;
                     temp = array[j];
                     array[j] = array[j - 1];
