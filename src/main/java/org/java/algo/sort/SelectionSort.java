6420fd8dce1f66d0d0821c1aa4ce47350b0f36fb
Hrishikesh <hrishishinde.p@gmail.com>
2014-02-28 11:00:33 +0530

    Added new shoarthand sort method

--- src/main/java/org/java/algo/sort/SelectionSort.java
+++ src/main/java/org/java/algo/sort/SelectionSort.java
@@ -24,15 +24,16 @@ public class SelectionSort {
 		return array;
 	}
 	
-	/*Method swap the content without using third 'temp' variable*/
+	/*Method swap the content without using third variable 'temp'*/
+	/*For python swapping is very simple. like array[i], array[j] = array[j], array[i]*/
 	public int[] shortSelectionSort(int[] array) {
 
 		for(int i = 0; i < array.length; i++) {
 			for (int j = array.length-1; j > i; j--) {
 				if(array[i] > array[j]) {
-					array[i] ^= array[j];
-					array[j] = array[i] ^ array[j];
-					array[i] ^= array[j];
+					array[i] ^= array[j];   			// example array[i] = 20, array[j] = 10, here result is array[i] = 30   
+					array[j] = array[i] ^ array[j];		// Here result is array[j] = 20 
+					array[i] ^= array[j];				// Here result is array[i] = 10
 				}
 			}
 		}
--- src/test/java/org/java/algo/sort/SelectionSortTest.java
+++ src/test/java/org/java/algo/sort/SelectionSortTest.java
@@ -10,6 +10,7 @@ public class SelectionSortTest {
 		int testCase[] = {3, 5, 2, 9, 1};
 		SelectionSort sorter = new SelectionSort();
 		int expected[] = {1, 2, 3, 5, 9};
+		
 		Assert.assertArrayEquals(expected, sorter.selectionSort(testCase));
 		Assert.assertArrayEquals(expected, sorter.shortSelectionSort(testCase));
 	}
